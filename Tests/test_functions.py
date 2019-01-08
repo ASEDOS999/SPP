4c3d9d2fe65a05a22120267883f459a2308da245
ASEDOS999 <32515384+ASEDOS999@users.noreply.github.com>
2019-01-08 18:18:10 +0200

    Add files via upload

--- Tests/test_functions.py
+++ Tests/test_functions.py
@@ -4,37 +4,58 @@ from math import pi, sqrt, cos
 import numpy as np
 
 class sinuses():
-    def __init__(self, a):
+    def __init__(self, a, b):
         self.parameters = a
-        self.min = - (a[0][0] + a[0][1])
-        
+        self.coef = b
+        self.min = self.calculate_function(b[0]/2, b[1]/2)
+
     def calculate_function(self, x, y):
         a = self.parameters
-        z = -a[0][0] * np.sin(x * pi / sqrt(2)) - a[0][1] * np.sin(y * pi / sqrt(2))
+        b = self.coef
+        z = -a[0][0] * np.sin(x * pi / b[0]) - a[0][1] * np.sin(y * pi / b[1])
         for i in range(1, len(a)):
-            z +=  (-a[i][0] * np.sin(x * pi / sqrt(2)) - a[i][1] * np.sin(y * pi / sqrt(2)) + a[i][0] + a[i][1] + a[i][2])**(i+1)
+            z +=  (-a[i][0] * np.sin(x * pi / b[0]) - a[i][1] * np.sin(y * pi / b[1]) + a[i][0] + a[i][1] + a[i][2])**(i+1)
         return z
     
-    def derivative_x(self, x, y):
+    def der_x(self, x, y):
         a = self.parameters
+        b = self.coef
         der = -a[0][0]
         for i in range(1, len(a)):
-            der += -(i+1) * a[i][0] * (-a[i][0] * np.sin(x * pi / sqrt(2)) - a[i][1] * np.sin(y * pi / sqrt(2)) + a[i][0] + a[i][1] + a[i][2])**i
-        return der * cos(x * pi / sqrt(2)) * pi / sqrt(2)
-        
-    def derivative_y(self, x, y):
+            der += -(i+1) * a[i][0] * (-a[i][0] * np.sin(x * pi / b[0]) - a[i][1] * np.sin(y * pi / b[1]) + a[i][0] + a[i][1] + a[i][2])**i
+        return der * cos(x * pi / b[0]) * pi / b[0]
+
+    def min_der_x(self, x):
         a = self.parameters
+        b = self.coef
+        der = a[0][0]
+        for i in range(1, len(a)):
+            der += (i+1) * a[i][0] * a[i][2]**i
+        return der * cos(x * pi / b[0]) * pi / b[0]
+    
+    def der_y(self, x, y):
+        a = self.parameters
+        b = self.coef
         der = -a[0][1]
         for i in range(1, len(a)):
             der += -(i+1) * a[i][1] * (-a[i][0] * np.sin(x * pi / sqrt(2)) - a[i][1] * np.sin(y * pi / sqrt(2)) + a[i][0] + a[i][1] + a[i][2])**i
-        return der * cos(y * pi / sqrt(2)) * pi / sqrt(2)
-    
-    def derivarive_xy(self, x, y):
+        return der * cos(y * pi / b[1]) * pi / b[1]
+
+    def min_der_y(self, y):
         a = self.parameters
+        b = self.coef
+        der = a[0][1]
+        for i in range(1, len(a)):
+            der += (i+1) * a[i][1] * a[i][2]**i
+        return der * cos(y * pi / b[1]) * pi / b[1]
+
+    def max_der_xy(self, x, num):
+        a = self.parameters
+        b = self.coef
         der = 0
         for i in range(1, len(a)):
-            der += i * (i+1) * (-a[i][0] * np.sin(x * pi / sqrt(2)) - a[i][1] * np.sin(y * pi / sqrt(2)) + a[i][0] + a[i][1]+ a[i][2])**(i-1)
-        return der * pi**2 / 2 * cos(y * pi / sqrt(2)) * cos(y * pi / sqrt(2))
+            der += i * (i+1) * (a[i][0] + a[i][1]+ a[i][2])**(i-1)
+        return der * pi**2 / (b[0] * b[1])
     
     def gradient(self, x, y):
         return (self.derivative_x(x, y), self.derivative_y(x, y))
