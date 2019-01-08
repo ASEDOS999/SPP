6720e9f2d255a7b93775618c75f0ff009f98c472
ASEDOS999 <32515384+ASEDOS999@users.noreply.github.com>
2019-01-08 09:09:45 +0200

    Add code of function for a method

---
+++ Tests/method_functions.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+def grad_discent_segment(segm, deriv, alpha_0, delta):
+    N = 1
+    x, x_prev = segm[0], (segm[0] + segm[1]) / 2
+    while abs(x - x_prev) > delta / 2:
+        x = min(max(x_prev - alpha_0 / N * deriv(x_prev), segm[0]), segm[1])
+        N += 1
+    return x
+
+def method(f, der_x, der_y, eps, f_min, Q, min_der_x, min_der_y, max_der_xy, N = 1):
+    x_0 = grad_discent_segment([Q[0], Q[1]], 
+                             lambda x: der_x(x, (Q[2] + Q[3]) / 2), 
+                             (Q[0] + Q[1]) / 4, 
+                             min_der_y((Q[2] + Q[3]) / 2) / max_der_xy)
+    der = der_y(x_0, (Q[2] + Q[3]) / 2)
+    if der == 0 or abs(f(x_0, (Q[2] + Q[3]) / 2) - f_min) <  eps:
+        return ((x_0, (Q[2] + Q[3]) / 2), N)
+    if der > 0:
+        Q[2], Q[3] = Q[2],  (Q[2] + Q[3]) / 2
+    else:
+        Q[3], Q[2] = Q[3],  (Q[2] + Q[3]) / 2
+    
+    y_0 = grad_discent_segment([Q[2], Q[3]], 
+                             lambda y: der_y((Q[0] + Q[1]) / 2, y), 
+                             (Q[2] + Q[3]) / 4, 
+                             min_der_x((Q[0] + Q[1]) / 2) / max_der_xy)
+    der = der_x((Q[0] + Q[1]) / 2, y_0)
+    if der == 0 or abs(f((Q[0] + Q[1]) / 2, y_0) - f_min) <  eps:
+        return (((Q[0] + Q[1]) / 2, y_0), N)
+    if der > 0:
+        Q[0], Q[1] = Q[0],  (Q[0] + Q[1]) / 2
+    else:
+        Q[1], Q[0] = Q[1],  (Q[0] + Q[1]) / 2
+    return method(f, der_x, der_y, eps, f_min, Q, min_der_x, min_der_y, max_der_xy, N + 1)
