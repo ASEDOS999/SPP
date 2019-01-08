b52f5503f336f6e4d0d6c03f38e8bfce84813ed3
ASEDOS999 <32515384+ASEDOS999@users.noreply.github.com>
2019-01-08 18:14:55 +0200

    Change a function 'mehod'
    Change arguments number and remove a recursion.

--- Tests/method_functions.py
+++ Tests/method_functions.py
@@ -8,28 +8,30 @@ def grad_discent_segment(segm, deriv, alpha_0, delta):
         N += 1
     return x
 
-def method(f, der_x, der_y, eps, f_min, Q, min_der_x, min_der_y, max_der_xy, N = 1):
-    x_0 = grad_discent_segment([Q[0], Q[1]], 
-                             lambda x: der_x(x, (Q[2] + Q[3]) / 2), 
-                             (Q[0] + Q[1]) / 4, 
-                             min_der_y((Q[2] + Q[3]) / 2) / max_der_xy)
-    der = der_y(x_0, (Q[2] + Q[3]) / 2)
-    if der == 0 or abs(f(x_0, (Q[2] + Q[3]) / 2) - f_min) <  eps:
-        return ((x_0, (Q[2] + Q[3]) / 2), N)
-    if der > 0:
-        Q[2], Q[3] = Q[2],  (Q[2] + Q[3]) / 2
-    else:
-        Q[3], Q[2] = Q[3],  (Q[2] + Q[3]) / 2
-    
-    y_0 = grad_discent_segment([Q[2], Q[3]], 
-                             lambda y: der_y((Q[0] + Q[1]) / 2, y), 
-                             (Q[2] + Q[3]) / 4, 
-                             min_der_x((Q[0] + Q[1]) / 2) / max_der_xy)
-    der = der_x((Q[0] + Q[1]) / 2, y_0)
-    if der == 0 or abs(f((Q[0] + Q[1]) / 2, y_0) - f_min) <  eps:
-        return (((Q[0] + Q[1]) / 2, y_0), N)
-    if der > 0:
-        Q[0], Q[1] = Q[0],  (Q[0] + Q[1]) / 2
-    else:
-        Q[1], Q[0] = Q[1],  (Q[0] + Q[1]) / 2
-    return method(f, der_x, der_y, eps, f_min, Q, min_der_x, min_der_y, max_der_xy, N + 1)
+def method(f, eps, Q):
+    N = 1
+    while True:
+        N += 1
+        x_0 = grad_discent_segment([Q[0], Q[1]], 
+                                 lambda x: f.der_x(x, (Q[2] + Q[3]) / 2), 
+                                 (Q[0] + Q[1]) / 4, 
+                                 f.min_der_y((Q[2] + Q[3]) / 2) / f.max_der_xy((Q[2] + Q[3]) / 2, 1)))
+        der = f.der_y(x_0, (Q[2] + Q[3]) / 2)
+        if der == 0 or abs(f.calculate_function(x_0, (Q[2] + Q[3]) / 2) - f.min) <  eps:
+            return ((x_0, (Q[2] + Q[3]) / 2), N)
+        if der > 0:
+            Q[2], Q[3] = Q[2],  (Q[2] + Q[3]) / 2
+        else:
+            Q[3], Q[2] = Q[3],  (Q[2] + Q[3]) / 2
+        
+        y_0 = grad_discent_segment([Q[2], Q[3]], 
+                                 lambda y: f.der_y((Q[0] + Q[1]) / 2, y), 
+                                 (Q[2] + Q[3]) / 4, 
+                                 f.min_der_x((Q[0] + Q[1]) / 2) / f.max_der_xy((Q[0] + Q[1]) / 2, 0))))
+        der = f.der_x((Q[0] + Q[1]) / 2, y_0)
+        if der == 0 or abs(f.calculate_function((Q[0] + Q[1]) / 2, y_0) - f.min) <  eps:
+            return (((Q[0] + Q[1]) / 2, y_0), N)
+        if der > 0:
+            Q[0], Q[1] = Q[0],  (Q[0] + Q[1]) / 2
+        else:
+            Q[1], Q[0] = Q[1],  (Q[0] + Q[1]) / 2
