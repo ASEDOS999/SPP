2150c18f685edd4e744ece3cf06394af9d295bd1
ASEDOS999 <32515384+ASEDOS999@users.noreply.github.com>
2019-01-10 05:57:32 +0200

    Add class for new test's functions
    Class 'almost_polinomial' is a class for not twice differentiable test's function with Lipshitz gradient. Full description of it and its properties you can find in file 'Desctiption.pdf'

--- Tests/test_functions.py
+++ Tests/test_functions.py
@@ -27,7 +27,8 @@ class sinuses():
         b = self.coef
         der = -a[0][0]
         for i in range(1, len(a)):
-            der += -(i+1) * a[i][0] * (-a[i][0] * np.sin(x * pi / b[0]) - a[i][1] * np.sin(y * pi / b[1]) + a[i][0] + a[i][1] + a[i][2])**i
+            der += (-(i+1) * a[i][0] * (-a[i][0] * np.sin(x * pi / b[0]) -
+            a[i][1] * np.sin(y * pi / b[1]) + a[i][0] + a[i][1] + a[i][2])**i)
         return der * cos(x * pi / b[0]) * pi / b[0]
 
     def min_der_x(self, x):
@@ -43,7 +44,8 @@ class sinuses():
         b = self.coef
         der = -a[0][1]
         for i in range(1, len(a)):
-            der += -(i+1) * a[i][1] * (-a[i][0] * np.sin(x * pi / sqrt(2)) - a[i][1] * np.sin(y * pi / sqrt(2)) + a[i][0] + a[i][1] + a[i][2])**i
+            der += (-(i+1) * a[i][1] * (-a[i][0] * np.sin(x * pi / b[0]) -
+            a[i][1] * np.sin(y * pi/b[1]) + a[i][0] + a[i][1] + a[i][2])**i)
         return der * cos(y * pi / b[1]) * pi / b[1]
 
     def min_der_y(self, y):
@@ -63,4 +65,60 @@ class sinuses():
         return der * pi**2 / (b[0] * b[1])
     
     def get_est(self, x, num):
-        return self.min_der_y(x) / self.max_der_xy()
+        if num == 0:
+            est = self.min_der_x(x)
+        else:
+            est = self.min_der_y(x)
+        return est / self.max_der_xy()
+    
+class almost_polinomial():
+    def __init__(self, a, Q):
+        self.min = 0
+        self.coef = a
+        self.Q = Q
+        self.min_x = 0
+        self.min_y = 0
+        self.L_grad = (3 * (a[0]+a[1]) + 
+                       2 * (abs(a[2]) + abs(a[3]))*(abs(a[2])*max(abs(Q[0]), abs(Q[1])) + abs(a[3])*max(abs(Q[3]), abs(Q[2]))))
+    
+    def psi(self, x):
+        if x < 0:
+            return 1.5
+        return 1
+    
+    def der_x(self, x, y):
+        return self.psi(x) * x + 2 * self.coef[2] * (self.coef[2] * x + self.coef[3] * y)
+    
+    def der_y(self, x, y):
+        return self.psi(y) * y + 2 * self.coef[3] * (self.coef[2] * x + self.coef[3] * y)
+    
+    def min_der_x(self, x):
+        flag = - (self.psi(x) + self.coef[2]) * x / (2 * self.coef[2] * self.coef[3])
+        if flag >= self.Q[2] and flag <= self.Q[3]:
+            return 0
+        flag = ((self.psi(x) + self.coef[2]) * x + 
+                2 * self.coef[2] * self.coef[3] * self.Q[3])
+        if flag < 0:
+            return flag
+        else:
+            return ((self.psi(x) + self.coef[2]) * x + 
+                    2 * self.coef[2] * self.coef[3] * self.Q[2])
+            
+    def min_der_y(self, y):
+        flag = - (self.psi(y) + self.coef[3]) * y / (2 * self.coef[2] * self.coef[3])
+        if flag >= self.Q[0] and flag <= self.Q[1]:
+            return 0
+        flag = ((self.psi(y) + self.coef[3]) * y + 
+                2 * self.coef[2] * self.coef[3] * self.Q[1])
+        if flag < 0:
+            return flag
+        else:
+            return ((self.psi(y) + self.coef[3]) * y + 
+                    2 * self.coef[2] * self.coef[3] * self.Q[0])
+            
+    def get_est(self, x, num):
+        if num == 0:
+            est = self.min_der_x(x)
+        else:
+            est = self.min_der_y(x)
+        return est / self.L_grad
