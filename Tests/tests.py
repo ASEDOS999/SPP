34d349f942a9ce75a1b314c309444fe0a0f8b23e
ASEDOS999 <32515384+ASEDOS999@users.noreply.github.com>
2019-01-17 21:43:03 +0200

    Сomparison of GD and new method
    Сomparison of GD and new method on random quadratic function

--- Tests/tests.py
+++ Tests/tests.py
@@ -3,8 +3,10 @@
 from time import time
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 from test_functions import sinuses
-from method_functions import method, gradient_descent, halving_square
+from test_functions import quadratic_function as qf
+from method_functions import halving_square, gradient_descent
 import math
 
 #Tests for iterations number
@@ -19,8 +21,8 @@ for i in np.linspace(1.1, 1.9, 5).tolist():
         while m != -1:
             f = sinuses(a, [i, j])
             for eps in epsilon:
-                N = method(f, eps, [0, 1, 0, 1])
-                results.append((N[1], eps, N[2], f.L))
+                N = halving_square(f, eps, [0, 1, 0, 1])
+                results.append((N[1], eps, f.L))
             m, n = 1, 2
             while m != -1 and a[m][n] == 1:
                 a[m][n] = 0.1
@@ -34,7 +36,7 @@ for i in np.linspace(1.1, 1.9, 5).tolist():
             a[max(m, 0)][n] *= 10
         num += 1
 print('100 % is completed')
-plt.plot([math.log(i[3] / i[1] / math.sqrt(2), 2) for i in results], [i[0] for i in results], 'ro')
+plt.plot([math.log(i[2] / i[1] / math.sqrt(2), 2) for i in results], [i[0] for i in results], 'ro')
 plt.plot([0, 17], [0, 17], 'b')
 plt.legend(('Tests functions', r'Line $N = \log \frac{La}{\sqrt{2}\epsilon}$'))
 plt.ylabel(r'$N$')
@@ -55,7 +57,7 @@ for i in np.linspace(1.1, 1.9, 5).tolist():
             f = sinuses(a, [i, j])
             for eps in epsilon:
                 m1 = time()
-                res_1 = gradient_descent(f.calculate_function, [0, 1, 0, 1], f.gradient, eps, 0.25)
+                res_1 = gradient_descent(f.calculate_function, [0, 1, 0, 1], f.gradient, eps, 0.25, f.min)
                 m2 = time()
                 res_2 = halving_square(f, eps, [0, 1, 0, 1])
                 m3 = time()
@@ -74,7 +76,6 @@ for i in np.linspace(1.1, 1.9, 5).tolist():
         num += 1
 print('100 % is completed')
 
-N = 5
 p = 1.05
 
 data = (len([i[0] for i in results
@@ -87,8 +88,55 @@ data = (len([i[0] for i in results
              if i[1] >= 0 and i[2] >= 0 and i[4] < 1/p * i[3]]),
         len([i[0] for i in results
              if i[1] < 0 and i[2] >= 0]))
-ind = np.arange(N)
+ind = np.arange(5)
 width = 0.35
 p1 = plt.bar(ind, data, width)
 plt.ylabel('Number of tasks')
 plt.xticks(ind, ('T1', 'T2', 'T3', 'T4', 'T5'))
+plt.show()
+
+#Quadratic functions
+results = []
+epsilon = [0.1**(i) for i in range(3)]
+num = 0
+N = 1000
+n = 0
+while len(results) < 3 * N:
+        n += 1
+        if n % 100 == 0:
+            print(n / 10,'%')
+        param = np.random.uniform(-10, 10, 6)
+        param[2] =  abs(param[2])
+        f = qf(param)
+        size_1, size_2 = random.uniform(0.5, 1), random.uniform(0.5, 1)
+        x_1, y_1 = f.solution[0], f.solution[1]
+        Q = [x_1 - (1-size_1), x_1 + (1+size_1), y_1 - (1-size_2), y_1 + (1+size_2)]
+        for eps in epsilon:
+            m1 = time()
+            res_1 = gradient_descent(f.calculate_function, Q, f.gradient, eps, 0.25, f.min)
+            m2 = time()
+            res_2 = halving_square(f, eps, Q)
+            m3 = time()
+            results.append((eps, res_1[1], res_2[1], m2 - m1, m3 - m2))
+
+p = 1.05
+
+data = (len([i[0] for i in results
+             if i[1] >= 0 and i[2] < 0]),
+        len([i[0] for i in results
+             if i[1] >= 0 and i[2] >= 0 and i[4] > p * i[3]]),
+        len([i[0] for i in results
+             if i[1] >= 0 and i[2] >= 0 and (1/p * i[3]) <= i[4] and i[4] <= (p * i[3])]),
+        len([i[0] for i in results
+             if i[1] >= 0 and i[2] >= 0 and i[4] < 1/p * i[3]]),
+        len([i[0] for i in results
+             if i[1] < 0 and i[2] >= 0]),
+        len([i[0] for i in results
+             if i[1] < 0 and i[2] < 0]))
+ind = np.arange(6)
+width = 0.35
+p1 = plt.bar(ind, data, width)
+plt.title('QF')
+plt.ylabel('Number of tasks')
+plt.xticks(ind, ('T1', 'T2', 'T3', 'T4', 'T5', 'T6'))
+plt.show()
