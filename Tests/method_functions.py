e9c70c0ea10d4b4b1da6b2fa55e936210c9c1531
ased <kuruzov2014@mail.ru>
2019-03-14 10:58:20 +0300

    Change architecture of function halving_square
    Now this function take two additional arguments - fucntions for to solve task on vertical and horizontal segment. Also caused by it changes was added to other programs.

--- Tests/method_functions.py
+++ Tests/method_functions.py
@@ -3,70 +3,64 @@
 import math
 
 def grad_descent_segment(segm, deriv, delta):
-    N = 0
-    x, x_prev, alpha_0 = (segm[0] + segm[1]) / 2, (segm[0] + segm[1]) / 2, (segm[0] + segm[1]) / 4
-    if delta == 0:
-        delta = 0.01 * alpha_0
-        #delta = -1
-    if delta < 0:
-        return (x, N)
-    while (abs(x - x_prev) > delta and N < 1000) or (N == 0):
-        x, x_prev = min(max(x - alpha_0 / math.sqrt(N + 1) * deriv(x), 
-                            segm[0]), 
-                        segm[1]), x
-        N += 1
-    if N >= 1000:
-        N = -1
-    return (x, N)
+	N = 0
+	x, x_prev, alpha_0 = (segm[0] + segm[1]) / 2, (segm[0] + segm[1]) / 2, (segm[0] + segm[1]) / 4
+	if delta == 0:
+		delta = 0.01 * alpha_0
+	if delta < 0:
+		return x
+	while (abs(x - x_prev) > delta and N < 1000) or (N == 0):
+		x, x_prev = min(max(x - alpha_0 / math.sqrt(N + 1) * deriv(x), 
+				segm[0]), segm[1]), x
+		N += 1
+	if N >= 1000:
+		N = -1
+	return x
 
-def halving_square(f, eps, square):
-    Q = square.copy()
-    N = 0
-    minimum = f.min
-    x_0, y_0 = (Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2
-    if f.der_x(x_0, y_0) == 0 and f.der_y(x_0, y_0) == 0:
-        return ((x_0, y_0), N)
-    f_opt = f.calculate_function(x_0, y_0)
-    while True:
-        x_0 = grad_descent_segment([Q[0], Q[1]], 
-                                 lambda x: f.der_x(x, (Q[2] + Q[3]) / 2),
-                                 f.get_est((Q[2] + Q[3]) / 2, 1))[0]
-        der = f.der_y(x_0, (Q[2] + Q[3]) / 2)
-        if der == 0 and f.der_x(x_0, (Q[2] + Q[3]) / 2) == 0:
-            return ((x_0, (Q[2] + Q[3]) / 2), N)
-        if der > 0:
-            Q[2], Q[3] = Q[2],  (Q[2] + Q[3]) / 2
-        else:
-            Q[3], Q[2] = Q[3],  (Q[2] + Q[3]) / 2
-        
-        y_0 = grad_descent_segment([Q[2], Q[3]], 
-                                 lambda y: f.der_y((Q[0] + Q[1]) / 2, y),
-                                 f.get_est((Q[0] + Q[1]) / 2, 0))[0]
-        der = f.der_x((Q[0] + Q[1]) / 2, y_0)
-        if der == 0 and f.der_y((Q[0] + Q[1]) / 2, y_0) == 0:
-            return (((Q[0] + Q[1]) / 2, y_0), N)
-        if der > 0:
-            Q[0], Q[1] = Q[0],  (Q[0] + Q[1]) / 2
-        else:
-            Q[1], Q[0] = Q[1],  (Q[0] + Q[1]) / 2
-        N += 1
-        
-        x_0, y_0 = (Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2
-        f_opt = f.calculate_function(x_0, y_0) 
-        if N >= 1000 or abs(f_opt - minimum) < eps:
-            if N >= 1000:
-                N = -1
-            return ((x_0, y_0), N)
-        
+def halving_square(f, eps, square, solve_vert_segm, solve_hor_segm):
+	Q = square.copy()
+	N = 0
+	minimum = f.min
+	x_0, y_0 = (Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2
+	if f.der_x(x_0, y_0) == 0 and f.der_y(x_0, y_0) == 0:
+		return ((x_0, y_0), N)
+	f_opt = f.calculate_function(x_0, y_0)
+	while True:
+		x_0 = solve_hor_segm([Q[0], Q[1]], (Q[2]+ Q[3]) / 2)
+		der = f.der_y(x_0, (Q[2] + Q[3]) / 2)
+		if der == 0 and f.der_x(x_0, (Q[2] + Q[3]) / 2) == 0:
+			return ((x_0, (Q[2] + Q[3]) / 2), N)
+		if der > 0:
+			Q[2], Q[3] = Q[2],  (Q[2] + Q[3]) / 2
+		else:
+			Q[3], Q[2] = Q[3],  (Q[2] + Q[3]) / 2
+		
+		y_0 = solve_vert_segm([Q[2], Q[3]], (Q[0] + Q[1]) / 2)
+		der = f.der_x((Q[0] + Q[1]) / 2, y_0)
+		if der == 0 and f.der_y((Q[0] + Q[1]) / 2, y_0) == 0:
+			return (((Q[0] + Q[1]) / 2, y_0), N)
+		if der > 0:
+			Q[0], Q[1] = Q[0],  (Q[0] + Q[1]) / 2
+		else:
+			Q[1], Q[0] = Q[1],  (Q[0] + Q[1]) / 2
+		N += 1
+		
+		x_0, y_0 = (Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2
+		f_opt = f.calculate_function(x_0, y_0) 
+		if N >= 1000 or abs(f_opt - minimum) < eps:
+			if N >= 1000:
+				N = -1
+			return ((x_0, y_0), N)
+
 def gradient_descent(f, Q, grad, eps, step, minimum):
-    N = 0
-    x = [(Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2]
-    x_prev = [(Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2]
-    while (abs(f(x[0], x[1]) - f(x_prev[0], x_prev[1])) > eps and N < 1000) or (N == 0):
-        der = grad(x[0], x[1])
-        x[0], x_prev[0] = min(max(x[0] - step / math.sqrt(N+1) * der[0], Q[0]), Q[1]), x[0]
-        x[1], x_prev[1] = min(max(x[1] - step / math.sqrt(N+1) * der[1], Q[2]), Q[3]), x[1]
-        N += 1
-    if N >= 1000:
-        N = -1
-    return (x, N, abs(f(x[0], x[1]) - f(x_prev[0], x_prev[1])))
+	N = 0
+	x = [(Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2]
+	x_prev = [(Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2]
+	while (abs(f(x[0], x[1]) - f(x_prev[0], x_prev[1])) > eps and N < 1000) or (N == 0):
+		der = grad(x[0], x[1])
+		x[0], x_prev[0] = min(max(x[0] - step / math.sqrt(N+1) * der[0], Q[0]), Q[1]), x[0]
+		x[1], x_prev[1] = min(max(x[1] - step / math.sqrt(N+1) * der[1], Q[2]), Q[3]), x[1]
+		N += 1
+	if N >= 1000:
+		N = -1
+	return (x, N, abs(f(x[0], x[1]) - f(x_prev[0], x_prev[1])))
--- Tests/tests.py
+++ Tests/tests.py
@@ -7,6 +7,7 @@ import random
 from test_functions import sinuses
 from test_functions import quadratic_function as qf
 from method_functions import halving_square, gradient_descent
+from method_functions import grad_descent_segment as GD_s
 import math
 
 #Tests for iterations number
@@ -14,27 +15,29 @@ results = []
 epsilon = [0.1**(1 + i) for i in range(7)] + [0.5**(1 + i) for i in range(7)]
 num = 0
 for i in np.linspace(1.1, 1.9, 5).tolist():
-    for j in np.linspace(1.1, 1.9, 5).tolist():
-        print((num * 4), '% is completed')
-        a = [[0.1, 0.1], [0.1, 0.1, 0.1]]
-        m, n = 1, 2
-        while m != -1:
-            f = sinuses(a, [i, j])
-            for eps in epsilon:
-                N = halving_square(f, eps, [0, 1, 0, 1])
-                results.append((N[1], eps, f.L))
-            m, n = 1, 2
-            while m != -1 and a[m][n] == 1:
-                a[m][n] = 0.1
-                n = n - 1
-                if n < 0:
-                    m = m - 1
-                    if m > 0:
-                        n = 2
-                    else:
-                        n = 1
-            a[max(m, 0)][n] *= 10
-        num += 1
+	for j in np.linspace(1.1, 1.9, 5).tolist():
+		print((num * 4), '% is completed')
+		a = [[0.1, 0.1], [0.1, 0.1, 0.1]]
+		m, n = 1, 2
+		while m != -1:
+			f = sinuses(a, [i, j])
+			for eps in epsilon:
+				N = halving_square(f, eps, [0, 1, 0, 1], 
+					lambda segm, y: GD_s(segm, lambda x: f.der_x(x, y), f.get_est(y, 1)),
+					lambda segm, x: GD_s(segm, lambda y: f.der_y(x, y), f.get_est(x, 0)))
+				results.append((N[1], eps, f.L))
+			m, n = 1, 2
+			while m != -1 and a[m][n] == 1:
+				a[m][n] = 0.1
+				n = n - 1
+				if n < 0:
+					m = m - 1
+					if m > 0:
+						n = 2
+					else:
+						n = 1
+			a[max(m, 0)][n] *= 10
+		num += 1
 print('100 % is completed')
 plt.plot([math.log(i[2] / i[1] / math.sqrt(2), 2) for i in results], [i[0] for i in results], 'ro')
 plt.plot([0, 17], [0, 17], 'b')
@@ -49,45 +52,47 @@ results = []
 epsilon = [0.1**(3 + i) for i in range(7)]
 num = 0
 for i in np.linspace(1.1, 1.9, 5).tolist():
-    for j in np.linspace(1.1, 1.9, 5).tolist():
-        print((num * 4), '% is completed')
-        a = [[0.1, 0.1], [0.1, 0.1, 0.1]]
-        m, n = 1, 2
-        while m != -1:
-            f = sinuses(a, [i, j])
-            for eps in epsilon:
-                m1 = time()
-                res_1 = gradient_descent(f.calculate_function, [0, 1, 0, 1], f.gradient, eps, 0.25, f.min)
-                m2 = time()
-                res_2 = halving_square(f, eps, [0, 1, 0, 1])
-                m3 = time()
-                results.append((eps, res_1[1], res_2[1], m2 - m1, m3 - m2))
-            m, n = 1, 2
-            while m != -1 and a[m][n] == 1:
-                a[m][n] = 0.1
-                n = n - 1
-                if n < 0:
-                    m = m - 1
-                    if m > 0:
-                        n = 2
-                    else:
-                        n = 1
-            a[max(m, 0)][n] *= 10
-        num += 1
+	for j in np.linspace(1.1, 1.9, 5).tolist():
+		print((num * 4), '% is completed')
+		a = [[0.1, 0.1], [0.1, 0.1, 0.1]]
+		m, n = 1, 2
+		while m != -1:
+			f = sinuses(a, [i, j])
+			for eps in epsilon:
+				m1 = time()
+				res_1 = gradient_descent(f.calculate_function, [0, 1, 0, 1], f.gradient, eps, 0.25, f.min)
+				m2 = time()
+				res_2 = halving_square(f, eps, [0, 1, 0, 1], 
+					lambda segm, y: GD_s(segm, lambda x: f.der_x(x, y), f.get_est(y, 1)),
+					lambda segm, x: GD_s(segm, lambda y: f.der_y(x, y), f.get_est(x, 0)))
+				m3 = time()
+				results.append((eps, res_1[1], res_2[1], m2 - m1, m3 - m2))
+			m, n = 1, 2
+			while m != -1 and a[m][n] == 1:
+				a[m][n] = 0.1
+				n = n - 1
+				if n < 0:
+					m = m - 1
+					if m > 0:
+						n = 2
+					else:
+						n = 1
+			a[max(m, 0)][n] *= 10
+		num += 1
 print('100 % is completed')
 
 p = 1.05
 
 data = (len([i[0] for i in results
-             if i[1] >= 0 and i[2] < 0]),
-        len([i[0] for i in results
-             if i[1] >= 0 and i[2] >= 0 and i[4] > p * i[3]]),
-        len([i[0] for i in results
-             if i[1] >= 0 and i[2] >= 0 and (1/p * i[3]) <= i[4] and i[4] <= (p * i[3])]),
-        len([i[0] for i in results
-             if i[1] >= 0 and i[2] >= 0 and i[4] < 1/p * i[3]]),
-        len([i[0] for i in results
-             if i[1] < 0 and i[2] >= 0]))
+			 if i[1] >= 0 and i[2] < 0]),
+		len([i[0] for i in results
+			 if i[1] >= 0 and i[2] >= 0 and i[4] > p * i[3]]),
+		len([i[0] for i in results
+			 if i[1] >= 0 and i[2] >= 0 and (1/p * i[3]) <= i[4] and i[4] <= (p * i[3])]),
+		len([i[0] for i in results
+			 if i[1] >= 0 and i[2] >= 0 and i[4] < 1/p * i[3]]),
+		len([i[0] for i in results
+			 if i[1] < 0 and i[2] >= 0]))
 ind = np.arange(5)
 width = 0.35
 p1 = plt.bar(ind, data, width)
@@ -102,37 +107,39 @@ num = 0
 N = 1000
 n = 0
 while len(results) < 3 * N:
-        n += 1
-        if n % 100 == 0:
-            print(n / 10,'%')
-        param = np.random.uniform(-10, 10, 6)
-        param[2] =  abs(param[2])
-        f = qf(param)
-        size_1, size_2 = random.uniform(0.5, 1), random.uniform(0.5, 1)
-        x_1, y_1 = f.solution[0], f.solution[1]
-        Q = [x_1 - (1-size_1), x_1 + (1+size_1), y_1 - (1-size_2), y_1 + (1+size_2)]
-        for eps in epsilon:
-            m1 = time()
-            res_1 = gradient_descent(f.calculate_function, Q, f.gradient, eps, 0.25, f.min)
-            m2 = time()
-            res_2 = halving_square(f, eps, Q)
-            m3 = time()
-            results.append((eps, res_1[1], res_2[1], m2 - m1, m3 - m2))
+		n += 1
+		if n % 100 == 0:
+			print(n / 10,'%')
+		param = np.random.uniform(-10, 10, 6)
+		param[2] =  abs(param[2])
+		f = qf(param)
+		size_1, size_2 = random.uniform(0.5, 1), random.uniform(0.5, 1)
+		x_1, y_1 = f.solution[0], f.solution[1]
+		Q = [x_1 - (1-size_1), x_1 + (1+size_1), y_1 - (1-size_2), y_1 + (1+size_2)]
+		for eps in epsilon:
+			m1 = time()
+			res_1 = gradient_descent(f.calculate_function, Q, f.gradient, eps, 0.25, f.min)
+			m2 = time()
+			res_2 = halving_square(f, eps, [0, 1, 0, 1], 
+				lambda segm, y: GD_s(segm, lambda x: f.der_x(x, y), f.get_est(y, 1)),
+				lambda segm, x: GD_s(segm, lambda y: f.der_y(x, y), f.get_est(x, 0)))
+			m3 = time()
+			results.append((eps, res_1[1], res_2[1], m2 - m1, m3 - m2))
 
 p = 1.05
 
 data = (len([i[0] for i in results
-             if i[1] >= 0 and i[2] < 0]),
-        len([i[0] for i in results
-             if i[1] >= 0 and i[2] >= 0 and i[4] > p * i[3]]),
-        len([i[0] for i in results
-             if i[1] >= 0 and i[2] >= 0 and (1/p * i[3]) <= i[4] and i[4] <= (p * i[3])]),
-        len([i[0] for i in results
-             if i[1] >= 0 and i[2] >= 0 and i[4] < 1/p * i[3]]),
-        len([i[0] for i in results
-             if i[1] < 0 and i[2] >= 0]),
-        len([i[0] for i in results
-             if i[1] < 0 and i[2] < 0]))
+			 if i[1] >= 0 and i[2] < 0]),
+		len([i[0] for i in results
+			 if i[1] >= 0 and i[2] >= 0 and i[4] > p * i[3]]),
+		len([i[0] for i in results
+			 if i[1] >= 0 and i[2] >= 0 and (1/p * i[3]) <= i[4] and i[4] <= (p * i[3])]),
+		len([i[0] for i in results
+			 if i[1] >= 0 and i[2] >= 0 and i[4] < 1/p * i[3]]),
+		len([i[0] for i in results
+			 if i[1] < 0 and i[2] >= 0]),
+		len([i[0] for i in results
+			 if i[1] < 0 and i[2] < 0]))
 ind = np.arange(6)
 width = 0.35
 p1 = plt.bar(ind, data, width)
