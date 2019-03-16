eed1001f9c6e276ac40692a87f5b9a3ddeb2f898
ased <kuruzov2014@mail.ru>
2019-03-16 15:46:01 +0300

    Add changes caused by adding class for solvers.

--- Tests/Experiments/estimates.py
+++ Tests/Experiments/estimates.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from test_functions import quadratic_function as qf
-from method_functions import halving_square, gss
+from method_functions import main_solver
 import math
 
 #Quadratic functions
@@ -27,24 +27,23 @@ for eps in epsilon:
 		x_1, y_1 = f.solution[0], f.solution[1]
 		Q = [x_1 - (1-size_1), x_1 + (1+size_1), y_1 - (1-size_2), y_1 + (1+size_2)]
 		R = 2
-		L = 3 * max(abs(param[0]), abs(param[1]), param[2]) *  max(abs(Q[1]), abs(Q[3]))
-		M = (2 * param[0]**2 + 4 * abs(param[0] * param[1]) + 2 * param[1] ** 2 + 2 * param[2]) * 10
+		L = f.lipschitz_function(Q)
+		M = f.lipschitz_gradient(Q)
+		solver = main_solver(f, Q, eps)
 		m1 = time()
-		est = eps / (2 * M * R * (math.sqrt(2) + math.sqrt(5)) * ( - math.log(eps / (L * R * math.sqrt(2)), 2)))
-		res = halving_square(f, eps, Q, 
-			lambda segm, y: gss(lambda x: f.calculate_function(x,y), segm, abs(f.der_y(f.get_sol_hor(segm, y), y)) / L),
-			lambda segm, x: gss(lambda y: f.calculate_function(x,y), segm, abs(f.der_x(f.get_sol_vert(segm,x), x)) / L))
+		solver.init_help_function(stop_func = 'big_grad')
+		res_1 = solver.halving_square()
 		m2 = time()
-		est = eps / (2 * M * R * (math.sqrt(2) + math.sqrt(5)) * ( - math.log(eps / (L * R * math.sqrt(2)), 2)))
-		res_2 = halving_square(f, eps, Q, 
-			lambda segm, y: gss(lambda x: f.calculate_function(x,y), segm, est),
-			lambda segm, x: gss(lambda y: f.calculate_function(x,y), segm, est))
+		solver.init_help_function(stop_func = 'const_est')
+		#print(solver.stop)
+		res_2 = solver.halving_square()
 		m3 = time()
-		results.append((eps, res[1], m2-m1, res_2[1], m3-m2))
+		# solver.init_help_function(stop_func = 'stop_ineq')
+		# res_3 = solver.halving_square()
+		results.append((eps, res_1[1], m2-m1, res_2[1], m3-m2))
 	list_gss = [i[2] for i in results]
 	print('eps = ', eps)
 	print('Mean time (Little Big) = %.2fms'%(1000 * np.mean([i[2] for i in results])))
 	print('Mean time (Constant) = %.2fms'%(1000 * np.mean([i[4] for i in results])))
 	full_results = full_results + results
 	results = []
-
--- Tests/method_functions.py
+++ Tests/method_functions.py
@@ -4,36 +4,40 @@ import math
 import numpy as np
 
 class solver_segment:
-	def __init__(self, f, Q, eps, stop_func, solve_segm):
+	def __init__(self, f, Q, eps):
 		self.f = f
 		self.Q = Q.copy()
+		self.size = Q[1] - Q[0]
 		self.eps = eps
-		self.axis = axis
-		self.stop = None
+		self.stop = self.const_est
+		self.solve = self.gss
+		self.value = 0
+		self.axis = 'x'
+		self.segm = []
+
+	def init_help_function(self, stop_func = 'const_est', solve_segm = 'gss'):
 		if stop_func == 'big_grad':
 			self.stop = self.big_grad
 		if stop_func == 'stop_ineq':
 			self.stop = self.stop_ineq
 		if stop_func == 'true':
 			self.stop = self.always_true
-		if stop_func == 'cons_est':
+		if stop_func == 'const_est':
 			self.stop = self.const_est
-
+		
 		if solve_segm == 'gss':
 			self.solve = self.gss
 		if solve_segm == 'grad_desc':
 			self.solve = self.grad_descent_segment
 
-		self.value = 0
-		self.axis = 'x'
-
 	def big_grad(self, a, b):
+		arg = self.value
 		f = self.f
 		L = f.lipschitz_gradient(self.Q)
 		if self.axis == 'x':
-			return b - a <= abs(f.der_y(f.get_sol_hor(segm, arg), arg)) / L
+			return b - a <= abs(f.der_y(f.get_sol_hor(self.segm, arg), arg)) / L
 		if self.axis == 'y':
-			return b - a <= abs(f.der_x(arg, f.get_sol_vert(segm, arg))) / L
+			return b - a <= abs(f.der_x(arg, f.get_sol_vert(self.segm, arg))) / L
 	
 	def stop_ineq(self, a, b):
 		if self.axis == 'x':
@@ -51,13 +55,13 @@ class solver_segment:
 	def always_true(self, a, b):
 		return True
 	
-	def stop_est(self, a, b):
-		L = self.f.lipshitz_function(self.Q)
-		M = self.f.lipshitz_gradient(self.Q)
-		R = Q[1] - Q[0]
+	def const_est(self, a, b):
+		L = self.f.lipschitz_function(self.Q)
+		M = self.f.lipschitz_gradient(self.Q)
+		R = self.size
 		eps = self.eps
 		est = eps / (2 * M * R * (math.sqrt(2) + math.sqrt(5)) * ( - math.log(eps / (L * R * math.sqrt(2)), 2)))
-		return b - a <= est
+		return b - a <= 0.0001 * est
 
 	def grad_descent_segment(self):
 		segm = self.segm
@@ -82,7 +86,11 @@ class solver_segment:
 		return x
 	
 	def gss(self):
-		a, b = segm
+		if self.axis == 'x':
+			f = lambda x: self.f.calculate_function(x, self.value)
+		if self.axis == 'y':
+			f = lambda y: self.f.calculate_function(self.value, y)
+		a, b = self.segm
 		gr = (math.sqrt(5) + 1) / 2
 		c = b - (b - a) / gr
 		d = a + (b - a) / gr 
@@ -103,23 +111,23 @@ class solver_segment:
 class main_solver(solver_segment):
 	def add_cond(self, x, y):
 		eps = self.eps
-		if np.linalg.norm(self.f.gradient(x, y)) <= eps / ((self.Q[0] - self.Q[1]) * math.sqrt(2)):
+		if np.linalg.norm(self.f.gradient(x, y)) <= eps / (self.size * math.sqrt(2)):
 			return True
 	
 	def halving_square(self):
-		Q = square.copy()
 		eps = self.eps
+		Q = self.Q.copy()
 		N = 0
 		minimum = self.f.min
 		x_0, y_0 = (Q[0] + Q[1]) / 2, (Q[2] + Q[3]) / 2
-		if add_cond(x_0, y_0):
+		if self.add_cond(x_0, y_0):
 			return ((x_0, y_0), N)
-		f_opt = f.calculate_function(x_0, y_0)
+		f_opt = self.f.calculate_function(x_0, y_0)
 		while True:
 			y_0 = (Q[2] + Q[3]) / 2
-			self.axis, self.value = 'x', y_0
-			x_0 = solve()
-			if add_cond(x_0, y_0):
+			self.axis, self.value, self.segm = 'x', y_0, [Q[0], Q[1]]
+			x_0 = self.solve()
+			if self.add_cond(x_0, y_0):
 				return (x_0, y_0)
 			der = self.f.der_y(x_0, y_0)
 			if der == 0 and self.f.der_x(x_0, y_0) == 0:
@@ -130,9 +138,9 @@ class main_solver(solver_segment):
 				Q[3], Q[2] = Q[3],  y_0
 			
 			x_0 = (Q[2] + Q[3]) / 2
-			self.axis, self.value = 'y', x_0
-			y_0 = solve()
-			if add_cond(x_0, y_0):
+			self.axis, self.value, self.segm = 'y', x_0, [Q[2], Q[3]]
+			y_0 = self.solve()
+			if self.add_cond(x_0, y_0):
 				return (x_0, y_0)
 			der = self.f.der_x(x_0, y_0)
 			if der == 0 and self.f.der_y(x_0, y_0) == 0:
