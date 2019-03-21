c9c25ff36969dc5802512146c0427054d0ff486b
ased <kuruzov2014@mail.ru>
2019-03-22 00:26:53 +0300

    Add ellipsoids method

--- Tests/method_functions.py
+++ Tests/method_functions.py
@@ -191,3 +191,23 @@ def gradient_descent(f, Q, grad, eps, step, minimum):
 	if N >= 1000:
 		N = -1
 	return (x, N, abs(f(x[0], x[1]) - f(x_prev[0], x_prev[1])))
+
+def ellipsoid(f, Q, x_0=None, rho=None, eps=None):
+	n = 2
+	x = np.zeros(2) if x_0 is None else x_0
+	eps = 5e-2 if eps is None else eps
+	rho = 1 if rho is None else rho
+	H = np.identity(n)
+	
+	q = n * (n - 1) ** (-(n-1) / (2*n)) * (n + 1) ** (-(n+1) / (2*n))
+	
+	N = 0
+	while abs(f.calculate_function(x[0], y[0]) - f.min) > eps and N < 10000:
+		gamma = (rho / (n+1)) * (n / np.sqrt(n ** 2 - 1)) ** k
+		_df = f.gradient(x[0], x[1])
+		x = np.clip(x_0 - gamma * H @ _df, *np.array(D).T)
+		H -= (2 / (n + 1)) * ((H @ _df @ _df.T * H) / ((H @ _df).T @ _df))
+		N += 1
+	if N >= 10000:
+		N = -1
+	return (x, N)
