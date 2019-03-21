be5e92647783466562cedb12ead6d91745f0ff3d
ased <kuruzov2014@mail.ru>
2019-03-21 23:19:25 +0300

    Add comparison of strategies speed
    Now one can see how there are many cases where one method is faster than the other.

--- Tests/Experiments/estimates.py
+++ Tests/Experiments/estimates.py
@@ -11,14 +11,11 @@ from test_functions import quadratic_function as qf
 from method_functions import main_solver
 import math
 
-def get_tests_estimates(epsilon):
-	results = []
-	num = 0
+def get_tests_estimates(epsilon, num_compare = False):
 	N = 1000
-	n = 0
-	full_results = []
 	for eps in epsilon:
 		for j in range(N):
+			results = []
 			param = np.random.uniform(-10, 10, 6)
 			param[2] =  abs(param[2])
 			f = qf(param)
@@ -42,6 +39,7 @@ def get_tests_estimates(epsilon):
 			results.append((eps, res_1[1], m2-m1, res_2[1], m3-m2, res_3[1], m4 - m3))
 		name = ['True gradient', 'Constant estimate', 'Current gradient']
 		print('eps = ', "{:.1e}".format(eps))
+		list_compare = []
 		for j in range(3):
 			list = [i[2 + j * 2] for i in results if i[1 + j * 2] >= 0]
 			print('Mean time (%s) = %.2fms'%(name[j], 1000 * np.mean(list)))
@@ -49,9 +47,12 @@ def get_tests_estimates(epsilon):
 			q = len(list)
 			if q > 0:
 				print("%s: Number of failed tests equals %d"%(name[j], q))
-		full_results = full_results + results
-		results = []
+		if num_compare:
+			list_n = [(i, j, k) for i in range(3) for j in range(3) for k in range(3) if i != j and j != k and i != k]
+			for n in list_n:
+				ind = len([i for i in results if i[2 + n[0] * 2] <= i[2 + n[1] * 2] <= i[2 + n[2] * 2]])
+				print("'%s' <= '%s' <= '%s': %d"%(name[n[0]], name[n[1]], name[n[2]], ind))
 
 if __name__ == "__main__":
 	eps = [0.1**(i) for i in range(7)]
-	get_tests_estimates(eps)
+	get_tests_estimates(eps, num_compare = True)
