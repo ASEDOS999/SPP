950167baa2ce0c56474cbd2f27b69c59e1799447
ased <kuruzov2014@mail.ru>
2019-05-17 10:10:32 +0300

    Trash excess function num_iter_tests and function for reasearching function solve

--- Tests/Experiments/comparison.py
+++ Tests/Experiments/comparison.py
@@ -15,43 +15,6 @@ from method_functions import gradient_descent
 from method_functions import ellipsoid
 import math
 
-#Tests for iterations number
-def num_iter_tests(epsilon):
-	results = []
-	num = 0
-	Q = [0, 1, 0, 1]
-	for i in np.linspace(1.1, 1.9, 5).tolist():
-		for j in np.linspace(1.1, 1.9, 5).tolist():
-			a = [[0.1, 0.1], [0.1, 0.1, 0.1]]
-			m, n = 1, 2
-			while m != -1:
-				f = sinuses(a, [i, j])
-				for eps in epsilon:
-					solver = main_solver(f, Q, eps)
-					solver.init_help_function(stop_func = 'true')
-					N = solver.halving_square()
-					results.append((N[1], eps, f.L))
-				m, n = 1, 2
-				while m != -1 and a[m][n] == 1:
-					a[m][n] = 0.1
-					n = n - 1
-					if n < 0:
-						m = m - 1
-						if m > 0:
-							n = 2
-						else:
-							n = 1
-				a[max(m, 0)][n] *= 10
-			num += 1
-	plt.plot([math.log(i[2] / i[1] / math.sqrt(2), 2) for i in results], [i[0] for i in results], 'ro')
-	plt.plot([0, 17], [0, 17], 'b')
-	plt.title("Iterations number")
-	plt.grid()
-	plt.legend(('Tests functions', r'Line $N = \log \frac{La}{\sqrt{2}\epsilon}$'))
-	plt.ylabel(r'Iterations Number')
-	plt.xlabel(r'$\log \frac{La}{\sqrt{2}\epsilon}$')
-	plt.show()
-
 def qf_comparison(epsilon = 1e-6, out = True):
 	param = np.random.uniform(-100, 100, 6)
 	param[2] =  abs(param[2])
@@ -61,7 +24,6 @@ def qf_comparison(epsilon = 1e-6, out = True):
 	Q = [x_1 - (1-size_1), x_1 + (1+size_1), y_1 - (1-size_2), y_1 + (1+size_2)]
 	results = comparison(f, Q, epsilon)
 	if out:
-		#norm_gradient = lambda x: np.linalg.norm(f.gradient(x[0], x[1]))
 		norm_gradient = lambda x: f.calculate_function(x[0], x[1]) - f.min
 		plt.semilogy([i for i in range(len(results[0]))], [norm_gradient(i) for i in results[0]])
 		plt.semilogy([i for i in range(len(results[2]))], [norm_gradient(i) for i in results[2]])
@@ -123,27 +85,6 @@ def qf_test_2(eps):
 	print('Theoretical Iteration Number through function constant', np.ceil(q_1))
 	print('Theoretical Iteration Number through gradient constant', np.ceil(q_2))
 	return res
-
-def LSM_comparison(epsilon = 1e-6, out = True):
-	a = random.uniform(4, 8)
-	b = random.uniform(-2, 2)
-	f = LSM_exp(a, b, 10)
-	Q = [4, 8, -2, 2]
-	results = comparison(f, Q, epsilon)
-	if out:
-		norm_gradient = lambda x: f.calculate_function(x[0], x[1]) - f.min
-		plt.semilogy([i for i in range(len(results[0]))], [norm_gradient(i) for i in results[0]])
-		plt.semilogy([i for i in range(len(results[2]))], [norm_gradient(i) for i in results[2]])
-		plt.semilogy([i for i in range(len(results[4]))], [norm_gradient(i) for i in results[4]])
-		plt.xlabel('Number of iterations')
-		plt.ylabel('Norm of gradient')
-		plt.xticks([i for i in range(0, 101, 10)])
-		plt.grid()
-		plt.legend(['Gradiend Descent', 'Halving Square', 'Ellipsoid'])
-		print('Gradiend Descent %.4f'%(results[1]))
-		print('Halving Square %.4f'%(results[3]), results[2][-1], a, b)
-		print('Ellipsoid %.4f'%(results[5]), results[4][-1])
-	return results
 
 def comparison(f, Q, eps):
 	n = 0
