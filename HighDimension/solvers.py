41726a3572d67e337692726ed44eff689a8644ff
ASEDOS999 <kuruzov2014@mail.ru>
2019-10-28 02:46:34 +0300

    Fix updating of square

--- HighDimension/solvers.py
+++ HighDimension/solvers.py
@@ -6,23 +6,22 @@ class HalvingCube:
 		self.methods = {
 				'GD' : self.GD
 				}
-	def GD(self, grad, x0, L, mu, cond):
+	def GD(self, grad, x0, L, mu, cond, proj):
 		x, R = x0
 		R0 = R
+		R = R0
 		k = 0
-		if mu > 0:
-			M = L/mu
-			q = (M-1)/(M+1)
-		else:
-			q = np.inf
-		while not cond(x, R):
-			x = x - 1/L * grad(x)
-			R = R0/(k+4) if R0/(k+4) < R * q else R * q
+		M = L/mu
+		alpha = 2/(L+mu)
+		q = (M-1)/(M+1)
+		while not (cond(x, R) or np.linalg.norm(grad(x))<=1e-15):
+			x = (x - alpha * grad(x))
+			R *= q
 			k += 1
 		return x
 	
 	def CurGrad(self, x, der, size, L):
-		return der(x)/L > size
+		return abs(der(x)/L) >= size
 
 	def solver(self, ind, new_Q, method = 'GD'):
 		start_point = self.f.get_start_point(ind, new_Q)
@@ -32,11 +31,14 @@ class HalvingCube:
 							  [new_Q[ind]] +
 							  [i for ind_, i in enumerate(x) if ind_ >= ind])
 		self.x_new = x_new
-		der = lambda x: self.f.grad[ind](x_new(x))
+		Q_ = [i for ind_,i in enumerate(new_Q) if ind_!= ind]
+		proj = lambda x: np.clip(x, *np.array(Q_).T)
+		der = lambda x: self.f.get_grad(x_new(x))[ind]
 		grad = lambda x: self.f.get_grad(x_new(x), without = ind)
 		mu = self.f.mu[ind]
 		cond = lambda x, size: self.CurGrad(x, der, size, L_x)
-		return der(solver(grad, start_point, L, mu, cond))
+		x = solver(grad, start_point, L, mu, cond, proj)
+		return der(x)
 
 	def main(self, Q = None, N = 10):
 		if Q is None:
@@ -44,26 +46,28 @@ class HalvingCube:
 		n = 0
 		while n < N:
 			n += 1
-			for ind,i in enumerate(Q):
+			ind = 0
+			while ind < len(Q):
 				new_Q = Q.copy()
 				new_Q[ind] = sum(Q[ind]) / 2
 				g = self.solver(ind, new_Q)
 				if g > 0:
-					Q[ind] = [Q[ind][0], new_Q[ind]]
+					new_Q[ind] = [Q[ind][0], new_Q[ind]]
 				else:
-					Q[ind] = [new_Q[ind], Q[ind][1]]
+					new_Q[ind] = [new_Q[ind], Q[ind][1]]
+				Q = new_Q
+				ind += 1
 		return np.array([sum(i)/2 for i in Q])
 
 
-def gradient_descent(f, Q, grad, L, eps, minimum, N = 100):
-	N = 0
-	x0 = np.array([sum(i) for i in Q])
+def gradient_descent(f, Q, N = 100):
+	x0 = np.array([sum(i)/2 for i in Q])
 	grad = lambda x: f.get_grad(x)
-	L = sum(f.L_list)
+	L = f.L_full_grad
 	x = x0
 	n = 0
 	while n < N:
-		x = x - 1/L * grad(x)
+		x = x - 0.5/L * grad(x)
 		n += 1
 	return x
 
--- HighDimension/test_functions.py
+++ HighDimension/test_functions.py
@@ -7,10 +7,10 @@ class QuadraticFunction:
 			n = 100
 		A = np.random.uniform(-1, 1, (n,n))
 		self.A_ = A
-		A = A.T.dot(A)
+		A = A.T.dot(A)+ np.eye(n)*10
 		self.eig_value = np.linalg.eig(A)[0]
 		self.eig_value.sort()
-		b = np.random.uniform(-1, 1, (n,))
+		b = np.random.uniform(-10, 10, (n,))
 
 		self.A = A
 		self.b = b
@@ -18,19 +18,20 @@ class QuadraticFunction:
 		self.L_full_grad = self.eig_value[-1]
 		self.mu_full = self.eig_value[0]
 		self.L, self.mu, self.grad = [], [], []
+		self.grad = [lambda x:self.get_grad(x)[ind] for ind in range(self.n)]
 		self.get_params()
 		
 	def get_params(self):
-		self.grad = [lambda x:(2*self.A[ind,:].dot(x) - 2*self.b[ind]) for ind in range(self.n)]
 		for ind in range(self.n):
 			A = np.delete(np.delete(self.A, ind, 0), ind, 1)
 			eig = np.linalg.eig(A)[0]
 			eig.sort()
 			self.mu.append(eig[0])
-			self.L.append((self.L_full_grad, eig[-1])) 
+			self.L.append((2*np.linalg.norm(self.A[ind,:]), 2 * eig[-1])) 
 
 	def func_value(self, x):
-		return np.linalg.norm(self.A_.dot(x) - self.b)**2
+		#return np.linalg.norm(self.A_.dot(x) - self.b)**2
+		return x.T@self.A@x - 2*self.b.T@x
 	
 	def get_grad(self, x, without = None):
 		g = 2*self.A.dot(x) - 2 * self.b
@@ -49,5 +50,9 @@ class QuadraticFunction:
 		eig.sort()
 		lmin = eig[0]
 		lim = 2 * np.linalg.norm(b)/lmin
+		s = 0
+		for i in new_Q:
+			if type(i)==type(list()):
+				s += (i[0]-i[1])**2
 		return np.zeros((self.n-1,)), lim
 		
