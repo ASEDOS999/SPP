f2eff6f1701bacab065cc0f5a0f16abedbed8349
ASEDOS999 <kuruzov2014@mail.ru>
2019-12-21 10:48:58 +0300

    Add LogSumExp function

--- NetTech/server.py
+++ NetTech/server.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 import sys
 sys.path.append("../Tests")
@@ -6,4 +7,54 @@ sys.path.append("../Tests/Tests_functions")
 sys.path.append("../Tests/Experiments")
 import comparison, estimates
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = 'localhost'
+port = 8007
+s.bind((host, port))
+s.listen(1)
+conn, addr = s.accept()
+print('client is at', addr)
 
+while True:
+	data = conn.recv(1000000)
+	request = data.decode("utf8").split()
+	if request[0] == "break":
+		break
+	elif request[0] == "LogSumExp":
+		N, time_max, eps, C = None, None, None, None
+		for i in request[1:]:
+			args = i.split("=")
+			if args[0] == "N":
+				N = int(args[1])
+			if args[0] == "time_max":
+				N = float(args[1])
+			if args[0] == "eps":
+				N = float(args[1])
+			if args[0] == "C":
+				N = float(args[1])
+		if N is None:
+			N = 10
+		if time_max is None:
+			time_max = 0.2
+		if eps is None:
+			eps = 1e-3
+		if C is None:
+			C = 1
+		results, f = comparison.NEWcomparison_LogSumExp(N, time_max = time_max, eps = eps, C=C)
+		keys = list(results.keys())
+		ng = lambda x,y: f.calculate_function(x,y)
+		new_dict = dict()
+		for key in keys:
+			times = [i - results[key][3][0] for i in results[key][3][:40]]
+			f_value = [ng(i[0], i[1]) for i in results[key][2][:40]]
+			new_dict[key] = (times, f_value)
+		data = b""
+		with open("1.pickle", "wb") as f:
+			pickle.dump(new_dict, f)
+			f.close()
+		with open("1.pickle", "rb") as f:
+			data = f.read()
+			print(data)
+			f.close()
+	conn.send(data)
+conn.close()
---
+++ NetTech/user.py
@@ -0,0 +1,33 @@
+import socket
+import pickle
+import matplotlib.pylab as plt
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = 'localhost'
+port = 8007
+s.connect(('', port))
+
+while True:
+	i = input()
+	if i == "break":
+		s.send(bytes(i.encode("utf8")))
+		break
+	name_file = i.split()[-1]
+	s.send(bytes(i.encode("utf8")))
+	data = s.recv(1000000)
+	if i.split()[0] == "LogSumExp":
+		with open("res.pickle", "wb") as f:
+			f.write(data)
+			f.close()
+		with open("res.pickle", "rb") as f:
+			new_dict = pickle.load(f)
+			f.close()
+		keys = new_dict.keys()
+		for key in keys:
+			plt.plot(new_dict[key][0], new_dict[key][1])
+			plt.xlabel("Time")
+			plt.ylabel("Dual Problem")
+			plt.grid()
+		plt.show()
+	print('received', len(data), 'bytes')
+s.close()
