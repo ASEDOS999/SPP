3853ed6baf4cab162151bef46d5d846b7ea2bebd
ASEDOS999 <kuruzov2014@mail.ru>
2020-04-11 21:43:36 +0300

    Add FGM for internal problem

--- SPP/Solvers/GradientMethods.py
+++ SPP/Solvers/GradientMethods.py
@@ -18,7 +18,7 @@ def FGM_external(f, start_x, R, eps = 0.001, Q, history = {}, key = "FGM"):
 	domain = np.array(Q)
 	x = start_x
 	results = [(x.copy(), time.time())]
-	grad = lambda x: f.get_delta_grad(x, cond, R)
+	grad = lambda x: f.get_delta_grad(x, cond)
 	L = f.L_xx
 	mu = f.mu_x
 	alpha, A = 1, 1
@@ -42,6 +42,42 @@ def FGM_external(f, start_x, R, eps = 0.001, Q, history = {}, key = "FGM"):
 		N += 1
 		results.append((x.copy(), time.time()))
 		est = 2 * min(4 * L * R/N**2, L * R * np.exp(-N/2 * np.sqrt(mu/L)))
+		if est <= eps:
+			history[key] = results
+			return (x, N, results)
+
+def FGM_internal(f, start_x, R, ext_var, eps = 0.001, Q, history = {}, key = "FGM"):
+	# It is realization for FGM method to optimize Saddle-Point problem
+	# on external variables
+	N = 0
+	cond = cond_for_fgm(f, eps)
+	domain = np.array(Q)
+	x = start_x
+	results = [(x.copy(), time.time())]
+	grad = lambda x: f.grad_y(ext_var, x, cond)
+	L = f.L_y
+	mu = f.mu_y
+	alpha, A = 1, 1
+	u, v = 2*L, np.zeros(x.shape)
+	while True:
+		der = np.array(grad(x))
+		np.array(der)
+
+		y = x - 1/L * der
+		y = np.clip(y, *domain.T)
+
+		u += mu * alpha
+		v += alpha * (mu * x - der)
+		z = v/u
+		z = np.clip(z, *domain.T)
+		tau = alpha/A
+		x = tau * z + (1 - tau) * y
+
+		alpha = (L+mu*A)/(2*L) * (1 + np.sqrt(abs(1 + (4*L*A)/(L+mu*A))) )
+
+		N += 1
+		results.append((x.copy(), time.time()))
+		est = min(4 * L * R/N**2, L * R * np.exp(-N/2 * np.sqrt(mu/L)))
 		if est <= eps:
 			history[key] = results
 			return (x, N, results)
