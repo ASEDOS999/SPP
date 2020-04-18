7d9646f86e71d4f3d14d128c0ac9487a20a581d0
ASEDOS999 <kuruzov2014@mail.ru>
2020-04-19 01:20:43 +0300

    Fix time_max

--- SPP/comparison.py
+++ SPP/comparison.py
@@ -7,13 +7,13 @@ from Solvers import GradientMethods, Ellipsoids, HalvCube
 def create_methods_dict(f, start_x_fgm, R_fgm, Q, eps, history, time_max = 1):
 	methods = dict()
 	fgm = GradientMethods.FGM_external
-	methods["FGM"] = lambda : fgm(f, start_x_fgm, R_fgm, Q, eps = eps, history = history, time_max = 1)
+	methods["FGM"] = lambda : fgm(f, start_x_fgm, R_fgm, Q, eps = eps, history = history, time_max = time_max)
 	
 	ellipsoids = Ellipsoids.delta_ellipsoid
-	methods["Ellipsoids"] = lambda: ellipsoids(f, Q, eps = eps, history = history, time_max = 1)
+	methods["Ellipsoids"] = lambda: ellipsoids(f, Q, eps = eps, history = history, time_max = time_max)
 	
 	dichotomy = HalvCube.Dichotomy(history = history)
-	methods["Dichotomy"] = lambda: dichotomy.Halving(f, Q, eps, time_max = 1)
+	methods["Dichotomy"] = lambda: dichotomy.Halving(f, Q, eps, time_max = time_max)
 	return methods
 	
 def method_comparison(methods = None):
