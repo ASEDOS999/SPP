f4d536ef98d08012bde4ff4862d70edb10dc43af
ASEDOS999 <kuruzov2014@mail.ru>
2020-05-25 03:27:47 +0300

    Fix start point of FGM

--- SPP/Solvers/GradientMethods.py
+++ SPP/Solvers/GradientMethods.py
@@ -8,12 +8,16 @@ def cond_for_fgm(f, eps, R):
 	L = f.L_xx
 	k = min(np.sqrt(4 * L * R/ eps), 2 * np.sqrt(f.L_xx/f.mu_x) * np.log(L * R/ eps))
 	z = min(1/3 * k+ 2.4, 1 + np.sqrt(f.L_xx/f.mu_x))
-	return lambda y, R : f.L_xx * R <= eps / (2 * z)
+	def cond(y, R):
+		#print("Cond", f.L_xx * R, eps / (2 * z), z, eps)
+		return f.L_xx * R <= eps / (2 * z)
+	return cond
 
 def FGM_external(f, start_x, R, Q, eps = 0.001, history = {}, key = "FGM", time_max = None):
 	# It is realization for FGM method to optimize Saddle-Point problem
 	# on external variables
 	N = 0
+	print(eps)
 	cond = cond_for_fgm(f, eps, R)
 	domain = np.array(Q)
 	x = start_x
@@ -22,11 +26,13 @@ def FGM_external(f, start_x, R, Q, eps = 0.001, history = {}, key = "FGM", time_
 	L = f.L_xx
 	mu = f.mu_x
 	alpha, A = 1, 1
-	u, v = 2*L, np.zeros(x.shape)
+	u, v = 2*L, 2*x
 	while True:
-		der = np.array(grad(x))
+		s = time.time()
+		der = grad(x)
+		print(time.time()-s)
 		np.array(der)
-
+		#print(x, der)
 		y = x - 1/L * der
 		y = np.clip(y, *domain.T)
 
@@ -35,9 +41,11 @@ def FGM_external(f, start_x, R, Q, eps = 0.001, history = {}, key = "FGM", time_
 		z = v/u
 		z = np.clip(z, *domain.T)
 		tau = alpha/A
+		#print(z, y)
+		#print()
 		x = tau * z + (1 - tau) * y
-
 		alpha = (L+mu*A)/(2*L) * (1 + np.sqrt(abs(1 + (4*L*A)/(L+mu*A))) )
+		A += alpha
 
 		N += 1
 		history[key].append((x.copy(), time.time()))
@@ -58,7 +66,8 @@ def FGM_internal(func,
 	N = 0
 	x, R = start_point
 	alpha, A = 1, 1
-	u, v = 2*L, np.zeros(x.shape)
+	u, v = 2*L, 2*x
+	#print(R)
 	while True:
 		der = np.array(grad(x))
 		np.array(der)
@@ -72,9 +81,10 @@ def FGM_internal(func,
 		x = tau * z + (1 - tau) * y
 
 		alpha = (L+mu*A)/(2*L) * (1 + np.sqrt(abs(1 + (4*L*A)/(L+mu*A))) )
-
+		A += alpha
 		N += 1
 		est = min(4 * L * R/N**2, L * R * np.exp(-N/2 * np.sqrt(mu/L)))
 		if cond(x, np.sqrt(est/mu)):
+			#print(N, np.sqrt(est/mu))
 			return (x, np.sqrt(est/mu))
 
