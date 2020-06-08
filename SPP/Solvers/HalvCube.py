41304ffffe5514b5c710bb83ecbcd6f5ed412a24
ASEDOS999 <kuruzov2014@mail.ru>
2020-06-08 22:38:07 +0300

    Add internal Dichotomy

--- SPP/Solvers/HalvCube.py
+++ SPP/Solvers/HalvCube.py
@@ -154,7 +154,7 @@ class Dichotomy_exact:
 		self.key = key
 		self.indexes = {}
 		
-	def help_task(self, f, Q, ind):
+	def help_task(self, Q, ind):
 		Q = list(Q)
 		Q_new = Q[:ind] + Q[ind+1:]
 		return Q_new
@@ -215,15 +215,21 @@ class Dichotomy_exact:
 	def Est(self, eps, g, R):
 		return max(self.Est1(g), self.Est2(eps, g, R))/2
 	
-	def Halving(self, f, Q, eps, indexes = {}, time_max = None):
+	def Halving(self, func, 
+					   grad,
+					   L, mu, eps,
+					   start_point = None,
+					   cond = None,
+					   Q = None,
+					   indexes = {}):
 		if self.L == np.infty:
 			# There was not initialization
-			self.f = f
-			self.L = f.L_xx
-			self.M = f.M_x
+			self.L = L
+			x, R = start_point
+			Q = np.hstack([x + R * np.ones(x.shape), x - R * np.ones(x.shape)])
 			self.n = len(Q)
 			self.Q = Q.copy()
-			self.mu = f.mu_x
+			self.mu = mu
 			Q_ = np.array(Q)
 			self.R = np.linalg.norm(Q_[:,0] - Q_[:,1])/2
 			self.history[self.key] = [((Q_[:,0] + Q_[:,1])/2, time.time())]
@@ -235,16 +241,15 @@ class Dichotomy_exact:
 		while True:
 			for ind, i in enumerate(Q):
 				# Fix coordinat with index 'ind'
-				Q_new = self.help_task(f, Q, ind)
+				Q_new = self.help_task(Q, ind)
 				# The new function of reconstructing x
 				reconstruct, new_indexes, true_ind = self.fix(sum(i)/2, ind, indexes.copy())
 				
 				# Solution of the new problem through this method
-				x, Delta  = self.Halving(f, Q_new, self.get_new_eps(eps), new_indexes)
+				x, Delta  = self.Halving(func, grad, L, mu, self.get_new_eps(eps), Q = Q_new, indexes = new_indexes)
 
 				# Calculate delta-subgradient
-				grad = f.get_delta_grad(reconstruct(x),
-									 self.cond(reconstruct(x), Q, eps, true_ind))
+				grad = grad(reconstruct(x))
 				g = grad[true_ind]
 				x_ = list(x)
 				x = x_[:ind] + [sum(i)/2] + x_[ind:]
@@ -263,11 +268,8 @@ class Dichotomy_exact:
 					# Update History
 					self.history[self.key].append((x, time.time()))
 					# Try condition
-					if self.M * R <= eps:
+					if cond(x, np.sqrt(Est/mu)):
 						return x, R
-					if not time_max is None:
-						if self.history[self.key][-1][1] - self.history[self.key][0][1] >time_max:
-							return x, R
 				
 				# Choice of multidimensional rectangle
 				c = sum(i)/2
