7833d968c966b0c7712fa87cde66bbecc789e128
ASEDOS999 <kuruzov2014@mail.ru>
2020-06-13 04:26:08 +0300

    Add final step

--- SPP/Solvers/HalvCube.py
+++ SPP/Solvers/HalvCube.py
@@ -36,9 +36,9 @@ class Dichotomy:
 			est = est(g)
 			if lipschitz_estimate / L <= est:
 				return True
-			s = min(min(x - Q_[:, 0]), min(Q_[:, 1] - x))
-			if s >= np.sqrt(R / (2 * L)):
-				return np.sqrt(L * R / 2) <= est
+			#s = min(min(x - Q_[:, 0]), min(Q_[:, 1] - x))
+			#if s >= np.sqrt(R / (2 * L)):
+			#	return np.sqrt(L * R / 2) <= est
 			return False
 		return lambda y, R: stop_cond(y, R, x, ind, est = est)
 	
@@ -77,6 +77,11 @@ class Dichotomy:
 	def Est(self, eps, g, R):
 		return max(self.Est1(g), self.Est2(eps, g, R))/2
 	
+	def final_cond(self, eps, add_cond = lambda *args: True):
+		def cond(y, R):
+			return add_cond(y) and self.f.M_y * R <= eps 
+		return cond
+	
 	def Halving(self, f, Q, eps, indexes = {}, time_max = None, stop_cond = lambda *args: False):
 		if self.L == np.infty:
 			# There was not initialization
@@ -90,9 +95,9 @@ class Dichotomy:
 			self.R = np.linalg.norm(Q_[:,0] - Q_[:,1])/2
 			self.history[self.key] = [((Q_[:,0] + Q_[:,1])/2, time.time())]
 		Q_ = np.array(Q)
-		if len(Q) == 0:
+		if len(Q) == 0 and self.n != 0:
 			# This set includes only one point
-			return [], 0
+			return [], 0, False
 		R = np.linalg.norm(Q_[:,0] - Q_[:,1])/2
 		while True:
 			for ind, i in enumerate(Q):
@@ -102,36 +107,22 @@ class Dichotomy:
 				reconstruct, new_indexes, true_ind = self.fix(sum(i)/2, ind, indexes.copy())
 				
 				# Solution of the new problem through this method
-				x, Delta  = self.Halving(f, Q_new, self.get_new_eps(eps), new_indexes)
-
+				x, Delta, stop  = self.Halving(f, Q_new, self.get_new_eps(eps), new_indexes)
+				
+				if stop:
+					if len(Q)==self.n:
+						return x, R
+					else:
+						return x, R, False
 				# Calculate delta-subgradient
 				grad, y = f.get_delta_grad(reconstruct(x),
 									 self.cond(reconstruct(x), Q, eps, true_ind))
 				g = grad[true_ind]
 				x_ = list(x)
-				x = x_[:ind] + [sum(i)/2] + x_[ind:]
-				x = np.array(x)				
+				x_.insert(ind, sum(i)/2)
+				x = np.array(x_)				
 				if g == 0:
 					return x, 0
-				
-				# Try stop condition at point x
-				Est = self.Est(eps, g, R)
-				if len(Q) < self.n:
-					# It is not main problem
-					if Delta <= Est:
-						return x, R
-				if len(Q) == self.n:
-					# It is initial square
-					# Update History
-					self.history[self.key].append(((x,y), time.time()))
-					# Try condition
-					if self.M * R <= eps:
-						return x, R
-					if not time_max is None:
-						if self.history[self.key][-1][1] - self.history[self.key][0][1] >time_max:
-							return x, R
-					if stop_cond(x, y):
-						return x, R
 				
 				# Choice of multidimensional rectangle
 				c = sum(i)/2
@@ -141,7 +132,29 @@ class Dichotomy:
 					Q[ind] = [c, i[1]]
 				# Update estimation of distance to point solution
 				Q_ = np.array(Q)
-				R = np.linalg.norm(Q_[:,0] - Q_[:,1])/2
+				R = np.linalg.norm(Q_[:,0] - Q_[:,1])		
+				# Try stop condition at point x
+				if len(Q) < self.n:
+					# It is not main problem
+					if R <= self.Est2(eps, g, R):
+						return x, R, True
+					if R <= self.Est1(g):
+						return x, R, False
+				if len(Q) == self.n:
+					# It is initial square
+					# Update History
+					self.history[self.key].append(((x,y), time.time()))
+					# Try condition
+					#if self.M * R <= eps:
+					#	return x, R
+					if not time_max is None:
+						if self.history[self.key][-1][1] - self.history[self.key][0][1] >time_max:
+							return x, R
+					if stop_cond(x, y):
+						grad, y = f.get_delta_grad(x,
+									 self.final_cond(eps, lambda y: stop_cond(x, y)))
+						self.history[self.key].append(((x, y), time.time()))
+						return x, R
 		return x, R
 
 class Dichotomy_exact:
@@ -235,6 +248,7 @@ class Dichotomy_exact:
 			Q_ = np.array(Q)
 			self.R = np.linalg.norm(Q_[:,0] - Q_[:,1])/2
 		Q_ = np.array(Q)
+		print(len(Q))
 		if len(Q) == 0:
 			# This set includes only one point
 			return [], 0
@@ -256,6 +270,7 @@ class Dichotomy_exact:
 				x = x_[:ind] + [sum(i)/2] + x_[ind:]
 				x = np.array(x)				
 				if g == 0:
+					print("Hello")
 					return x, 0
 
 				# Choice of multidimensional rectangle
@@ -272,13 +287,15 @@ class Dichotomy_exact:
 				Est = self.Est(eps, g, R)
 				if len(Q) < self.n:
 					# It is not main problem
+					if R<= self.Est2(eps,g,R):
+						print("Good")
 					if R <= Est:
+						print(self.Est1, self.Est2, Delta, R)
 						return x, R
 				if len(Q) == self.n:
 					# It is initial square
 					# Update History
 					# Try condition
 					if cond(x, R):
-						print("HI")
 						return x, R
 		return x, R
